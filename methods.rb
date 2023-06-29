119046722620d13ef05fce4dd2f095f74aa31a40
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-06-29 11:03:10 +0500

    add factorial method code

--- methods.rb
+++ methods.rb
@@ -1,3 +1,9 @@
 class Methods
+    def factorial(number)
+        return 'Error! Kindly input number greater than or
+        equal to zero' if number.negative?
+        return 1 if number.zero?
+        [1..number].reduce(:*)
+    end
 
 end
