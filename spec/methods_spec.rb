1897707a9fab71bd27e070ad26bbd6e2a21956d8
Muhammad Zunair khan <zunairkhan811@gmail.com>
2023-06-29 10:05:15 +0500

    add tests for factorial method

---
+++ spec/methods_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../methods'
+
+describe Methods do
+    before(:each) do
+        @methods = Methods.new()
+end
+
+context 'Testing the factorial method' do
+    it 'return 1 when the input number is 0' do
+        expect(@methods.factorial(0)).to eql 1
+    end
+
+    it 'return error when when input number is negative' do
+        expect(@methods.factorial(-2)).to eql 'Error! Kindly input number greater than or
+        equal to zero'
+    end
+
+    it 'return the factorial of a number greater than zero' do
+        expect(@methods.factorial(5)). to eql 120
+    end
+
+    it 'return the factorial of a number greater than zero' do
+        expect(@methods.factorial(10)). to eql 3628800
+    end
+end
